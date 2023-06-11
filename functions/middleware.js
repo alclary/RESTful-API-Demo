4ac22aabd71ed5768b8baf2a8ec7fe7488749511
LoganC <alclary@posteo.net>
2023-06-11 13:20:11 -0500

    jwt authentication middleware implemented

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,5 +1,24 @@
+require("dotenv").config();
 const { Validator } = require("express-json-validator-middleware");
 const { validate } = new Validator();
+const { expressjwt } = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// exprestjwt middleware config
+let jwtConfig = {
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 50,
+    jwksUri: `${process.env.AUTH_DOMAIN}/.well-known/jwks.json`,
+  }),
+  issuer: `${process.env.AUTH_DOMAIN}/`,
+  algorithms: ["RS256"],
+};
+
+// expressjwt middleware alternatives
+const requireJwtAuth = expressjwt({ ...jwtConfig });
+// const decodeJwtOnly = expressjwt({ ...jwtConfig, credentialsRequired: false });
 
 // JSON Schema for Boat
 const boatSchema = {
@@ -37,4 +56,5 @@ const loadSchema = {
 module.exports.middleware = {
   validateBoatSchema: validate({ body: boatSchema }),
   validateLoadSchema: validate({ body: loadSchema }),
+  requireJwtAuth,
 };
--- routes/boatRoutes.js
+++ routes/boatRoutes.js
@@ -5,18 +5,34 @@ const { middleware } = require("../functions/middleware");
 const router = Router();
 
 // GET - retrieve all boats with pagination
-router.get("/", boatController.boats_get);
+router.get("/", middleware.requireJwtAuth, boatController.boats_get);
 // GET - a single boat by ID
-router.get("/:boatId", boatController.boat_get);
-// GET - get all loads associated with boat
-router.get("/:boatId/loads", boatController.boat_loads_get);
+router.get("/:boatId", middleware.requireJwtAuth, boatController.boat_get);
 // POST - Create new boat
-router.post("/", middleware.validateBoatSchema, boatController.create_boat);
+router.post(
+  "/",
+  [middleware.requireJwtAuth, middleware.validateBoatSchema],
+  boatController.create_boat
+);
+// PATCH - update a given boat
+// TODO PATCH boat
 // PUT - add load to boat
-router.put("/:boatId/loads/:loadId", boatController.assign_load_to_boat);
+router.put(
+  "/:boatId/loads/:loadId",
+  middleware.requireJwtAuth,
+  boatController.assign_load_to_boat
+);
 // DELETE - remove load from boat
-router.delete("/:boatId/loads/:loadId", boatController.remove_load_from_boat);
+router.delete(
+  "/:boatId/loads/:loadId",
+  middleware.requireJwtAuth,
+  boatController.remove_load_from_boat
+);
 // DELETE - delete a given boat
-router.delete("/:boatId", boatController.delete_boat);
+router.delete(
+  "/:boatId",
+  middleware.requireJwtAuth,
+  boatController.delete_boat
+);
 
 module.exports = router;
